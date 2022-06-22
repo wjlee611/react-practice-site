050a97a6377f510081d8fc61ed8efe1acccb14df
wjlee611 <wjlee611@gmail.com>
2022-06-22 15:14:44 +0900

    initialize settings

---
+++ src/Router.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route></Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#353b48",
+  textColor: "#f5f6fa",
+  accentColor: "#fbc531",
+};
